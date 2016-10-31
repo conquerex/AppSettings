08e3c63d4f014343a47b55ec37004c68c8562e65
손종국 <conquerex@gmail.com>
2016-10-31 13:56:43 +0900

    Firebase Noti 기능

---
+++ Firebase_Message/app/src/main/java/com/example/jongkook/firebase_message/MyFirebaseInstanceIDService.java
@@ -0,0 +1,26 @@
+package com.example.jongkook.firebase_message;
+
+
+import android.util.Log;
+
+import com.google.firebase.iid.FirebaseInstanceId;
+import com.google.firebase.iid.FirebaseInstanceIdService;
+
+
+public class MyFirebaseInstanceIDService extends FirebaseInstanceIdService {
+
+    private static final String TAG = "MyFirebaseIIDService";
+    // [START refresh_token]
+    @Override
+    public void onTokenRefresh() {
+        // Get updated InstanceID token.
+        String refreshedToken = FirebaseInstanceId.getInstance().getToken();
+        Log.d(TAG, "Refreshed token: " + refreshedToken);
+        sendRegistrationToServer(refreshedToken);
+    }
+    // [END refresh_token]
+
+    private void sendRegistrationToServer(String token) {
+        // TODO: Implement this method to send token to your app server.
+    }
+}
